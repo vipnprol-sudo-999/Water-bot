22002d297e57959cbeb2677c5bb58bb0d44991b3
vipnprol-sudo-999 <nffgrhvcrz@privaterelay.appleid.com>
2025-11-07 13:23:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-REMINDER_HOURS = os.getenv("REMINDER_HOURS", "9,11,14,17,20")  # comma-separated hours
+REMINDER_HOURS = os.getenv("REMINDER_HOURS", "5,7,10,13,16")  # comma-separated hours
 DB_PATH = os.getenv("DB_PATH", "users.db")
 MESSAGES_FILE = os.getenv("MESSAGES_FILE", "messages.json")
 
